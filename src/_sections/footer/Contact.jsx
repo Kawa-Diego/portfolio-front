d7550b311aee909a7ac4377730365cf857ea42ae
Diego-mah-gah <kawadiego.soares@gmail.com>
2026-05-08 01:42:45 -0300

    feat: included some animations at footer section.

--- src/_sections/footer/Contact.jsx
+++ src/_sections/footer/Contact.jsx
@@ -9,10 +9,14 @@ export default function Contact() {
     const startValue = SplitText.create("#section-contact h4", {
       type: "chars",
     });
+    const startValue2 = SplitText.create("#section-contact p", {
+      type: "chars",
+    });
     const timeline = gsap.timeline({
       scrollTrigger: {
         trigger: "#section-contact",
         startValue,
+        startValue2,
         start: "top center",
       },
       ease: "power1.inOut",
@@ -24,16 +28,40 @@ export default function Contact() {
         yPercent: 50,
         stagger: 0.02,
       })
-      .from("#section-contact h4, #section-contact nav h4", {
+      .from(startValue2.chars, {
+        opacity: 0,
+        yPercent: 50,
+        stagger: 0.02,
+      })
+      .from("#section-contact p", {
+        y: "-30",
+        duration: 0.5,
+        ease: "power1.inOut",
+      })
+      .from("#section-contact h4", {
         y: "-30",
         duration: 0.5,
         ease: "power1.inOut",
       });
+
+    const socialButtons = gsap.utils.toArray("#contact-handling .social-btn");
+    socialButtons.forEach((button, index) => {
+      timeline.from(
+        button,
+        {
+          opacity: 0,
+          yPercent:-10,
+          duration: 0.45,
+          ease: "power1.inOut",
+        },
+        index === 0 ? "-=0.2" : "-=0.3"
+      );
+    });
   });
 
   return (
     <section className="section-contact" id="section-contact">
-      <div className="contact-container mx-auto flex justify-between gap-5">
+      <div className="contact-container mx-auto flex items-center justify-between gap-5">
         <div className="contact-describe">
           <div className="contact-describe-text-one">
             <h4>{contact.heading}</h4>
@@ -42,7 +70,7 @@ export default function Contact() {
             <p>{contact.contact.email + " • " + contact.contact.phone}</p>
           </div>
         </div>
-        <div className="contact-handling">
+        <div className="contact-handling" id="contact-handling">
           {socials.map((social) => (
             <a
               key={social.name}
