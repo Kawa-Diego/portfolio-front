19e749e8985edba84768cf6e71dd91b6834ea71f
Diego-mah-gah <kawadiego.soares@gmail.com>
2026-05-08 01:09:00 -0300

    feat: constants folder will store, as the name says, constants functions to be mapped in the code

---
+++ _constants/footer.jsx
@@ -0,0 +1,27 @@
+const socials = [
+    {
+        name: "Instagram",
+        icon: "../_images/instagram.svg",
+        url: "https://www.instagram.com"
+    },
+    {
+        name: "LinkedIn",
+        icon: "../_images/linkedin2.svg",
+        url: "https://www.linkedin.com"
+    },
+    {
+        name: "Whatsapp",
+        icon: "../_images/whatsapp.svg",
+        url: "https://www.web.whatsapp.com"
+    }
+]
+
+const contact = {
+    heading: "diego.dev",
+    contact: {
+        email: "email@email.com",
+        phone: "(44) 99999-9999"
+    }
+}
+
+export { socials, contact }
