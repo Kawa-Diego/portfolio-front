a5c779dc822982c2b3294f3912a6c56b1cca5cf8
Diego-mah-gah <kawadiego.soares@gmail.com>
2026-05-09 06:03:23 -0300

    feat: adding some new field to navbar section

--- _constants/navbar.jsx
+++ _constants/navbar.jsx
@@ -10,10 +10,14 @@ const navbar = {
       url: "#section-about",
     },
     {
-      name: "Skills",
-      url: "#section-skills",
-    }
-  ]
+      name: "Projetos",
+      url: "#section-projects",
+    },
+  ],
+  cta: {
+    name: "Contato",
+    url: "#section-contact",
+  },
 };
 
 export { navbar };
