5ff885d2adb2479cba3eac12cb1cbc5bdfb28cf3
Diego-mah-gah <kawadiego.soares@gmail.com>
2026-05-09 06:02:41 -0300

    feat: adding some new field to about section

--- _constants/about.jsx
+++ _constants/about.jsx
@@ -1,4 +1,76 @@
-const frontendSkills = ["React", "Next.js", "TypeScript", "Tailwind"];
-const backendSkills = ["Node.js", "PostgreSQL", "Docker", "Java", "Spring Boot"];
+const skills = [
+    {
+        skills: {
+            title: ["Frontend"],
+            skills: [
+                {
+                    name: "HTML5",
+                    icon: "../_images/html5.svg",
+                },
+                {
+                    name: "CSS3",
+                    icon: "../_images/css3.svg",
+                },
+                {
+                    name: "JavaScript",
+                    icon: "../_images/javascript.svg",
+                },
+                {
+                    name: "React",
+                    icon: "../_images/react.svg",
+                },
+            ],
+        },
+    },
+    {
+        skills: {
+            title: ["Backend"],
+            skills: [
+                {
+                    name: "Java",
+                    icon: "../_images/java.svg",
+                },
+                {
+                    name: "Typescript",
+                    icon: "../_images/typescript.svg",
+                }, 
+                {
+                    name: "Node.js",
+                    icon: "../_images/nodejs.svg",
+                },
+            ]
+        },
+    },
+    {
+        skills: {
+            title: ["Infraestrutura e DevOps"],
+            skills: [
+                {
+                    name: "Docker",
+                    icon: "../_images/docker.svg",
+                },
+                {
+                    name: "Kubernetes",
+                    icon: "../_images/kubernetes.svg",
+                }
+            ]
+        },
+    },
+    {
+        skills: {
+            title: ["Tools"],
+            skills: [
+                {
+                    name: "Git",
+                    icon: "../_images/git.svg",
+                },
+                {
+                    name: "GitHub",
+                    icon: "../_images/github.svg",
+                },
+            ],
+        },
+    },
+]
 
-export { frontendSkills, backendSkills };
+export { skills };
